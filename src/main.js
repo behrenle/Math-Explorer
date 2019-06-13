cb93c74d803d2bd0b84fd2f943de00eeb604717a
Lennard Behrens <behrenle@posteo.de>
2019-06-13 02:06:30 +0200

    import bootstrap and mathjs

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import 'bootstrap';
+import math from 'mathjs';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  math,
   render: h => h(App)
 }).$mount("#app");

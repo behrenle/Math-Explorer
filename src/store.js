7d9cd2bf3937bbf414b71ba48b350210d79d8285
Lennard Behrens <behrenle@posteo.de>
2019-09-21 13:30:02 +0200

    settings are stored in a cookie

--- src/store.js
+++ src/store.js
@@ -12,6 +12,9 @@ math.import({
 
 Vue.use(Vuex);
 
+const COOKIE_EXPIRE_DAYS = 32;
+const COOKIE_SETTINGS_NAME = "settings";
+
 function formatGerman2English(state, str) {
   if (state.decimalMode === "german") {
     return str
@@ -19,7 +22,45 @@ function formatGerman2English(state, str) {
       .replace(/;/g, ",")
       .replace(/\|/g, ";");
   } else {
-    return str
+    return str;
+  }
+}
+
+function saveSettings2Cookie(state) {
+  let settings = {
+    decimalMode: state.decimalMode,
+    inputMode: state.inputMode,
+    language: state.language
+  };
+  let d = new Date();
+  d.setTime(d.getTime() + COOKIE_EXPIRE_DAYS * 24 * 3600000);
+  let expires = "expires=" + d.toUTCString();
+  document.cookie =
+    COOKIE_SETTINGS_NAME +
+    "=" +
+    JSON.stringify(settings) +
+    ";" +
+    expires +
+    ";path=/";
+}
+
+function loadSettingsValue(vName) {
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let cookieParts = decodedCookie.split(";");
+  for (var i = 0; i < cookieParts.length; i++) {
+    let cookiePart = cookieParts[i];
+    while (cookiePart.charAt(0) === " ") {
+      cookiePart = cookiePart.substring(1);
+    }
+    if (cookiePart.indexOf(COOKIE_SETTINGS_NAME + "=") === 0) {
+      let settings = JSON.parse(
+        cookiePart.substring(
+          (COOKIE_SETTINGS_NAME + "=").length,
+          cookiePart.length
+        )
+      );
+      return settings[vName];
+    }
   }
 }
 
@@ -27,10 +68,18 @@ export default new Vuex.Store({
   state: {
     history: [],
     scope: {},
-    decimalMode: "german",
-    language: "english",
-    inputMode: "advanced",
-
+    decimalMode:
+      loadSettingsValue("decimalMode") != null
+        ? loadSettingsValue("decimalMode")
+        : "german",
+    language:
+      loadSettingsValue("language") != null
+        ? loadSettingsValue("language")
+        : "english",
+    inputMode:
+      loadSettingsValue("inputMode") != null
+        ? loadSettingsValue("inputMode")
+        : "advanced"
   },
   mutations: {
     EVALUATE_INPUT: (state, inputLine) => {
@@ -57,24 +106,27 @@ export default new Vuex.Store({
     },
     SWITCH_DECIMAL_MODE: state => {
       if (state.decimalMode === "german") {
-        state.decimalMode = "english"
+        state.decimalMode = "english";
       } else {
-        state.decimalMode = "german"
+        state.decimalMode = "german";
       }
+      saveSettings2Cookie(state);
     },
     SWITCH_LANGUAGE: state => {
       if (state.language === "german") {
-        state.language = "english"
+        state.language = "english";
       } else {
-        state.language = "german"
+        state.language = "german";
       }
+      saveSettings2Cookie(state);
     },
     SWITCH_INPUT_MODE: state => {
       if (state.inputMode === "simple") {
-        state.inputMode = "advanced"
+        state.inputMode = "advanced";
       } else {
-        state.inputMode = "simple"
+        state.inputMode = "simple";
       }
+      saveSettings2Cookie(state);
     }
   },
   actions: {}

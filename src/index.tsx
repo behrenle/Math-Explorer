e93cd3c1653a47b2bf97bea54216e595041f42c3
Lennard Behrens <behrenle@posteo.de>
2020-12-06 23:14:07 +0100

    manifest and service worker

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/app/App';
+import {register as registerServiceWorker} from "./serviceWorker";
 
+registerServiceWorker();
 ReactDOM.render(
     <React.StrictMode>
         <App/>

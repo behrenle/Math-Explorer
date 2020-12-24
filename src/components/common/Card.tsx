edf566b5be563314c6d8503ef07b8b1b46c07266
Lennard Behrens <behrenle@posteo.de>
2020-12-24 16:49:06 +0100

    - added show background image settings item - reorganized settings with <h2> headings

--- src/components/common/Card.tsx
+++ src/components/common/Card.tsx
@@ -1,18 +1,26 @@
 import styled from "styled-components";
 
 const Card = styled.section`
-    color: ${props => props.theme.card.color};    
-    background-color: ${props => props.theme.card.backgroundColor};
-    box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.1);
-    font-size: ${props => props.theme.fontSize.m};
+  color: ${props => props.theme.card.color};
+  background-color: ${props => props.theme.card.backgroundColor};
+  box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.1);
+  font-size: ${props => props.theme.fontSize.m};
 
-    & h1 {
-        font-size: ${props => props.theme.fontSize.l};
-        background-color: rgba(0, 0, 0, 0.08);
-        margin: 0;
-        padding: 10px 20px;
-    }
+  & h1 {
+    font-size: ${props => props.theme.fontSize.l};
+    font-weight: 400;
+    background-color: rgba(0, 0, 0, 0.08);
+    margin: 0;
+    padding: 10px 20px;
+  }
   
+  & h2 {
+    font-size: ${props => props.theme.fontSize.m};
+    font-weight: 400;
+    margin: 0.5em 0 0 0;
+    padding: 10px 20px;
+  }
+
   & a {
     color: inherit;
     font-weight: bolder;
--- src/components/views/settings/Settings.tsx
+++ src/components/views/settings/Settings.tsx
@@ -61,7 +61,7 @@ const Settings: React.FC = () => {
     return (
         <Container>
             <SettingsCard>
-                <h1>{t("settings.interface")}</h1>
+                <h1>{t("settings.categories.interface")}</h1>
                 <ul>
                     <SettingsItem
                         label="settings.language"
@@ -76,6 +76,14 @@ const Settings: React.FC = () => {
                         setter={value => updateInterfaceSettings({inputForm: value })}
                     />
                     <SettingsItem
+                        label="settings.copy_manual_content_on_click"
+                        options={enableOptions}
+                        value={settings.interfaceSettings.copyManualContentOnClick}
+                        setter={value => updateInterfaceSettings({copyManualContentOnClick: value === "true"})}
+                    />
+
+                    <h2>{t("settings.categories.advanced_input_form")}</h2>
+                    <SettingsItem
                         label="settings.show_cell_numbers"
                         options={enableOptions}
                         value={settings.interfaceSettings.showLineNumbers}
@@ -88,17 +96,13 @@ const Settings: React.FC = () => {
                         setter={value => updateInterfaceSettings({copyCellContentOnClick: value === "true"})}
                     />
                     <SettingsItem
-                        label="settings.copy_manual_content_on_click"
-                        options={enableOptions}
-                        value={settings.interfaceSettings.copyManualContentOnClick}
-                        setter={value => updateInterfaceSettings({copyManualContentOnClick: value === "true"})}
-                    />
-                    <SettingsItem
                         label="settings.cell_language_tag"
                         options={languageWithInheritOptions}
                         value={settings.interfaceSettings.cellLanguageTag}
                         setter={value => updateInterfaceSettings({cellLanguageTag: value})}
                     />
+
+                    <h2>{t("settings.categories.appearance")}</h2>
                     <SettingsItem
                         label="settings.theme"
                         options={themes.map(theme => {return {label: theme.toUpperCase(), value: theme}})}
@@ -111,10 +115,16 @@ const Settings: React.FC = () => {
                         value={settings.interfaceSettings.fontSize}
                         setter={value => updateInterfaceSettings({fontSize: value})}
                     />
+                    <SettingsItem
+                        label="settings.show_background_image"
+                        options={enableOptions}
+                        value={settings.interfaceSettings.showBackgroundImage}
+                        setter={value => updateInterfaceSettings({showBackgroundImage: value === "true" })}
+                    />
                 </ul>
             </SettingsCard>
             <SettingsCard>
-                <h1>{t("settings.calculator")}</h1>
+                <h1>{t("settings.categories.calculator")}</h1>
                 <ul>
                     <SettingsItem
                         label="settings.significant_decimal_places"

7693e3f0e75d427f90cdd3ea40d5c24a5155d9c8
Lennard Behrens <behrenle@posteo.de>
2020-12-25 06:20:27 +0100

    tracker test

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import AppWrapper from "./components/app/AppWrapper";
 import {register as registerServiceWorker} from "./serviceWorker";
 import "./googlefonts.css";
+import {initialize, event} from "react-ga";
+import {id as analyticsId} from "./analytics.json";
+
+initialize(analyticsId, {
+    debug: true,
+    gaOptions: {
+        siteSpeedSampleRate: 100
+    }
+});
+
+event({
+    action: "Test",
+    category: "Tests"
+});
 
 registerServiceWorker();
 ReactDOM.render(

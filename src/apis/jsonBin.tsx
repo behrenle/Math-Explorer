cda023f932ac8c1d35dd196b4ba35b55a2b1d167
Lennard Behrens <behrenle@posteo.de>
2021-01-10 16:05:44 +0100

    env test

--- src/apis/jsonBin.tsx
+++ src/apis/jsonBin.tsx
@@ -2,7 +2,6 @@ const baseUrl = "https://api.jsonbin.io";
 const collectionId = process.env.REACT_APP_JSON_BIN_COLLECTION as string;
 const apiKey = process.env.REACT_APP_JSON_BIN_API_KEY as string;
 
-console.log("debug info");
 console.log("apikey", apiKey);
 console.log("collectionid", collectionId);
 

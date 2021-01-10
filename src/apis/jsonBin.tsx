e12e3d0994cc673e83258ebafef67b3682e6d8d8
Lennard Behrens <behrenle@posteo.de>
2021-01-10 16:10:06 +0100

    removed logs

--- src/apis/jsonBin.tsx
+++ src/apis/jsonBin.tsx
@@ -2,9 +2,6 @@ const baseUrl = "https://api.jsonbin.io";
 const collectionId = process.env.REACT_APP_JSON_BIN_COLLECTION as string;
 const apiKey = process.env.REACT_APP_JSON_BIN_API_KEY as string;
 
-console.log("apikey", apiKey);
-console.log("collectionid", collectionId);
-
 export const fetchBin = async (id: string) => {
     return await fetch(`${baseUrl}/b/${id}`, {
         method: "GET",
